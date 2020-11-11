a815ef724350d0cb052f03aec096c0b57b235156
dans088 <daniel.atilano@live.com>
2020-11-11 15:06:20 -0600

    Add files via upload

---
+++ Ariadne_thread.rkt
@@ -0,0 +1,20 @@
+;Daniel Atilano
+;Rafael Díaz
+;4/12/2020
+;Final Project
+
+#lang racket
+
+
+(define (read-maze filename)
+    (define in (open-input-file filename))
+        (let loop
+            ([lines (read-line in 'any)]
+            [maze empty])
+            (if (eof-object? lines)
+                maze ;(car (car maze))
+                (loop (read-line in 'any) (append maze (for/list () (string-split lines " ")))))))
+
+
+
+

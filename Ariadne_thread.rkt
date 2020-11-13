3dfab4ced75a336b41fff02f3936842edc8a8e09
dans088 <daniel.atilano@live.com>
2020-11-13 13:48:35 -0600

    Update Ariadne_thread.rkt

--- Ariadne_thread.rkt
+++ Ariadne_thread.rkt
@@ -10,10 +10,12 @@
     (define in (open-input-file filename))
         (let loop
             ([lines (read-line in 'any)]
-            [maze empty])
-            (if (eof-object? lines)
-                maze ;(car (car maze))
-                (loop (read-line in 'any) (append maze (for/list () (string-split lines " ")))))))
+            [maze empty]
+            [size 0])
+            (cond 
+                [(eof-object? lines) (build-tree maze size)] ;call function
+                [else 
+                    (loop (read-line in 'any) (append maze (for/list () (string-split lines " "))) (add1 size))])))
 
 
 
